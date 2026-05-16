cc26458512342e51e9ca1cd3b5230abe0578ec8a
PietroBIN <peterguidorizzi@gmail.com>
2026-05-16 20:16:06 -0300

    Testing Builder

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
+import model.Category;
+import model.Item;
+import model.ItemBuilder;
+import model.Room;
+import service.ItemService;
+
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args){
+
+        ItemService service = new ItemService();
+
+        Item rice = new ItemBuilder()
+                .name("Arroz")
+                .description("5kg")
+                .room(Room.KITCHEN)
+                .category(Category.FOOD)
+                .quantity(1)
+                .build();
+
+        service.addItem(rice);
+
+        System.out.println(service.listItems());
+
+    }
 }
